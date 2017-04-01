6a3d6617303fc4e2d3d1a5987bfde172cf5e5791
Ítalo Garleni Rodríguez <italogar6@hotmail.com>
2017-04-01 18:31:55 +0200

    Gradient (magnitude and direction) and histograms
    gradient function calculates pixel gradients and its directions.
    Histograms functions divide image into 8x8p groups and calculate its
    gradient histogram

--- gradiente.m
+++ gradiente.m
@@ -6,8 +6,27 @@ function [magnitud,orientacion] = gradiente(patch)
     %OUT:
     %   -magnitud: matrix 128x64 with gradient of each pixel
     %   -orientacion: matrix 128x64 with gradient direction
+    patch = double(patch);
+    imSize = size(patch);
+    magnitud = zeros(imSize(1),imSize(2));
+    orientacion = zeros(imSize(1),imSize(2));
+    imSize(1) = imSize(1) - 1;
+    imSize(2) = imSize(2) - 1;
+    for i = 2:imSize(1)
+        for j = 2:imSize(2)
+            magnitudTemp = double([0,0,0]);
+            orientacionTemp = double([0,0,0]);
+            for k = 1:imSize(3)
+                dx = patch(i+1, j, k) - patch(i-1, j, k);
+                dy = patch(i, j+1, k) - patch(i, j-1, k);
+                magnitudTemp(k) = sqrt(double(dx^2 + dy^2));
+                orientacionTemp(k) = atan2(dy,dx);
+            end
+            indexMax = find(magnitudTemp==max(magnitudTemp));
+            indexMax = indexMax(1);
+            magnitud(i,j) = magnitudTemp(indexMax(1));
+            orientacion(i,j) = orientacionTemp(indexMax) * 180/pi ;
+        end
+    end
     
-    
-    
-    %Use artan2 MATLAB function
 end
