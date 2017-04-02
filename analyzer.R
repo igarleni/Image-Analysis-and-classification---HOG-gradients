8603327ae6e5a4f26221f8d93f086d0c137cfc0e
Ítalo Garleni Rodríguez <italogar6@hotmail.com>
2017-04-02 16:54:52 +0200

    Rstudio project created
    Added unnecessary files to .gitignore

---
+++ analyzer.R
@@ -0,0 +1,7 @@
+#
+
+testBackground <- read.csv("testBackground.csv", header = F)
+testPedestrian <- read.csv("testPedestrian.csv", header = F)
+trainBackground <- read.csv("trainBackground.csv", header = F)
+trainPedestrian <- read.csv("trainPedestrian.csv", header = F)
+
